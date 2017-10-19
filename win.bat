a54c640f9c2895b9c7ae92e6a8cc0a51eb620858
equiman <equiman@users.noreply.github.com>
2017-10-18 19:13:40 -0500

    Add resources files

---
+++ win.bat
@@ -0,0 +1,51 @@
+@echo off
+call refreshenv > nul
+cls
+set dbg=%1
+
+:update
+call color 06
+cls
+echo ==========================================================================================
+echo  UPDATE
+echo ==========================================================================================
+echo. 
+echo ==^> Updating...
+cd /d %~dp0
+git pull | findstr /c:"Already up-to-date"
+if %errorlevel% == 0 (
+    goto start
+) else (
+    cls
+    call color E0
+    git config --local core.filemode false
+    git reset --hard HEAD
+    git pull
+    echo. 
+    echo ==========================================================================================
+    echo. 
+    echo  HardHat was updated please RESTART to continue.
+    echo. 
+    echo  Refer to CHANGELOG file for details
+    echo  or visit https://github.com/equiman/hardhat/
+    echo. 
+    echo ==========================================================================================
+    echo.
+    pause
+    goto end
+)
+
+:start
+cls
+call color 07
+if defined dbg (
+    dotnet run
+) else (
+    HardHat.exe
+)
+cls
+
+:end
+cls
+call color 07
+exit /b
