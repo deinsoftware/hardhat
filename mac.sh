9b0caa971e2731d402365c84776a25aca6acce85
equiman <equiman@users.noreply.github.com>
2017-10-18 19:15:01 -0500

    Add resources files

---
+++ mac.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+clear
+resize -s 27 88
+clear
+
+function fxUpdate() {
+    cd ~/Applications/HardHat/
+    clear;
+    echo "======================================================================================="
+    echo " UPDATE "
+    echo "======================================================================================="
+    echo "";
+
+    echo " --> Updating... "
+    updated="$(git pull)"
+    if [ "${updated}" != "Already up-to-date." ]; then 
+        fxGit
+        echo ""
+        echo "======================================================================================="
+        echo ""
+        echo "HardHat was updated please RESTART to continue."
+        echo ""
+        echo "Refer to CHANGELOG file for details"
+        echo "or visit http://www.github.com/equiman/hardhat/"
+        echo ""
+        echo "======================================================================================="
+        pause "Press [Enter] key to continue..."
+        fxExit
+    else
+        fxStart
+    fi
+}
+
+function fxGit() {
+    clear
+    git config --local core.filemode false
+    git reset --hard HEAD
+    git pull
+}
+
+function fxStart() {
+    #Permissions
+    chmod +x mac.sh
+    chmod +x mac.command
+    chmod +x cmd.mac.sh
+    if [ -n "$1" ]; then
+        #Development
+        dotnet run
+    else
+        chmod +x HardHat
+        ./HardHat
+    fi
+    clear
+}
+
+function fxExit() {
+    clear
+    exit
+}
+
+function pause() {
+    echo ""
+    read -p " $*"
+}
+
+fxUpdate
