607764ac0f948fa19e24cfc7a7d25fa78d66c56a
equiman <equiman@users.noreply.github.com>
2018-01-12 15:55:29 -0500

    Secure SonarCloud token

--- sonar.bat
+++ sonar.bat
@@ -1,8 +1,8 @@
 @echo off
-
+set /p token=<sonar.txt
 :: https://docs.sonarqube.org/display/SONAR/Analysis+Parameters
-SonarQube.Scanner.MSBuild.exe begin /k:"dein:hardhat" /n:"HardHat" /v:"5.0.1" /o:"dein" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="34dadb57c7435b016b8ba12e48dc49f5d1eadee6" /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*"
+SonarQube.Scanner.MSBuild.exe begin /k:"dein:hardhat" /n:"HardHat" /v:"5.0.1" /o:"dein" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="%token%" /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*"
 dotnet restore
 dotnet build
 dotnet test HardHat.Tests/HardHat.Tests.csproj
-SonarQube.Scanner.MSBuild.exe end /d:sonar.login="34dadb57c7435b016b8ba12e48dc49f5d1eadee6"
+SonarQube.Scanner.MSBuild.exe end /d:sonar.login="%token%"
