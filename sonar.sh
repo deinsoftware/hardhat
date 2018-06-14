7e5b89a77d77771e3a93f6fb2ad5c3e0281ea322
equiman <equiman@users.noreply.github.com>
2018-06-14 10:29:45 -0500

    Sonar scanner update for mac

--- sonar.sh
+++ sonar.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 token="$(cat sonar.txt)"
 # https://docs.sonarqube.org/display/SONAR/Analysis+Parameters
-mono ~/Applications/Sonar/Scanner/msbuild/SonarQube.Scanner.MSBuild.exe begin /k:"dein:hardhat" /n:"HardHat" /v:"6.2.1" /o:"dein" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="${token}" /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*"
+dotnet ~/Applications/Sonar/Scanner/msbuild/SonarScanner.MSBuild.dll begin /k:"dein:hardhat" /n:"HardHat" /v:"6.2.1" /o:"dein" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="${token}" /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*"
 dotnet restore
 dotnet build
 dotnet test HardHat.Tests/HardHat.Tests.csproj
-mono ~/Applications/Sonar/Scanner/msbuild/SonarQube.Scanner.MSBuild.exe end /d:sonar.login="${token}"
+dotnet ~/Applications/Sonar/Scanner/msbuild/SonarScanner.MSBuild.dll end /d:sonar.login="${token}"
