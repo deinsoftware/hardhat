90cd738e0b3dba225ba29bd521d88217a8fa57d9
equiman <equiman@users.noreply.github.com>
2018-08-22 10:11:08 -0500

    Update sonarscanner command

--- sonar.bat
+++ sonar.bat
@@ -1,8 +1,8 @@
 @echo off
 set /p token=<sonar.txt
 :: https://docs.sonarqube.org/display/SONAR/Analysis+Parameters
-SonarQube.Scanner.MSBuild.exe begin /k:"dein:hardhat" /n:"HardHat" /v:"6.9.0" /o:"dein" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="%token%" /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*"
+dotnet sonarscanner begin /k:"dein:hardhat" /n:"HardHat" /v:"6.9.1" /o:"dein" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="%token%" /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*"
 dotnet restore
 dotnet build
 dotnet test HardHat.Tests/HardHat.Tests.csproj
-SonarQube.Scanner.MSBuild.exe end /d:sonar.login="%token%"
+dotnet sonarscanner end /d:sonar.login="%token%"
--- sonar.sh
+++ sonar.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 token="$(cat sonar.txt)"
 # https://docs.sonarqube.org/display/SONAR/Analysis+Parameters
-dotnet ~/Applications/Sonar/Scanner/msbuild/SonarScanner.MSBuild.dll begin /k:"dein:hardhat" /n:"HardHat" /v:"6.9.0" /o:"dein" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="${token}" /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*"
+dotnet sonarscanner begin /k:"dein:hardhat" /n:"HardHat" /v:"6.9.1" /o:"dein" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="${token}" /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*"
 dotnet restore
 dotnet build
 dotnet test HardHat.Tests/HardHat.Tests.csproj
-dotnet ~/Applications/Sonar/Scanner/msbuild/SonarScanner.MSBuild.dll end /d:sonar.login="${token}"
+dotnet sonarscanner end /d:sonar.login="${token}"
