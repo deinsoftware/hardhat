4346a7d74775c21f0f88b69a5bb2298079516676
equiman <equiman@users.noreply.github.com>
2018-08-26 20:45:38 -0500

    Update not installed message

--- HardHat/controller/Versions.cs
+++ HardHat/controller/Versions.cs
@@ -16,9 +16,9 @@ namespace HardHat
         {
             try
             {
-                Response result = _shell.Term($"gradle -v");
+                Response result = _shell.Term($"gradles -v");
                 string response = Strings.ExtractLine(result.stdout, "Gradle", "Gradle ");
-                _shell.Result(response, "Not Installed");
+                _shell.Result(response, "is not Installed");
             }
             catch (Exception Ex)
             {
@@ -32,7 +32,7 @@ namespace HardHat
             {
                 Response result = _shell.Term($"gulp --v");
                 string response = Strings.GetWord(result.stdout, 3);
-                _shell.Result(response, "Not Installed");
+                _shell.Result(response, "is not Installed");
             }
             catch (Exception Ex)
             {
@@ -46,7 +46,7 @@ namespace HardHat
             {
                 Response result = _shell.Term($"java -version 2>&1");
                 string response = Strings.ExtractLine(result.stdout, "java version", "java version ", "\"");
-                _shell.Result(response, "Not Installed");
+                _shell.Result(response, "is not Installed");
             }
             catch (Exception Ex)
             {
@@ -60,7 +60,7 @@ namespace HardHat
             {
                 Response result = _shell.Term($"node -v");
                 string response = Strings.RemoveWords(result.stdout, "v");
-                _shell.Result(response, "Not Installed");
+                _shell.Result(response, "is not Installed");
             }
             catch (Exception Ex)
             {
@@ -73,7 +73,7 @@ namespace HardHat
             try
             {
                 Response result = _shell.Term($"npm -v");
-                _shell.Result(result.stdout, "Not Installed");
+                _shell.Result(result.stdout, "is not Installed");
             }
             catch (Exception Ex)
             {
@@ -91,7 +91,7 @@ namespace HardHat
             {
                 Response result = _shell.Term($"ng -v");
                 string response = Strings.ExtractLine(result.stdout, "Angular CLI:", "Angular CLI: ");
-                _shell.Result(response, "Not Installed");
+                _shell.Result(response, "is not Installed");
             }
             catch (Exception Ex)
             {
@@ -104,7 +104,7 @@ namespace HardHat
             try
             {
                 Response result = _shell.Term($"cordova -v");
-                _shell.Result(result.stdout, "Not Installed");
+                _shell.Result(result.stdout, "is not Installed");
             }
             catch (Exception Ex)
             {
@@ -118,7 +118,7 @@ namespace HardHat
             {
                 Response result = _shell.Term($"git --version");
                 string response = Strings.GetWord(result.stdout, 2);
-                _shell.Result(response, "Not Installed");
+                _shell.Result(response, "is not Installed");
             }
             catch (Exception Ex)
             {
@@ -131,7 +131,7 @@ namespace HardHat
             try
             {
                 Response result = _shell.Term($"tns --version");
-                _shell.Result(result.stdout, "Not Installed");
+                _shell.Result(result.stdout, "is not Installed");
             }
             catch (Exception Ex)
             {
@@ -145,7 +145,7 @@ namespace HardHat
             {
                 Response result = _shell.Term($"tsc -v");
                 string response = Strings.RemoveWords(result.stdout, "Version ");
-                _shell.Result(response, "Not Installed");
+                _shell.Result(response, "is not Installed");
             }
             catch (Exception Ex)
             {
@@ -159,7 +159,7 @@ namespace HardHat
             {
                 Response result = _shell.Term($"sonar-scanner -v");
                 string response = Strings.ExtractLine(result.stdout, "INFO: SonarQube Scanner ", "INFO: SonarQube Scanner ");
-                _shell.Result(response, "Not Installed");
+                _shell.Result(response, "is not Installed");
             }
             catch (Exception Ex)
             {
@@ -172,7 +172,7 @@ namespace HardHat
             try
             {
                 Response result = _shell.Term($"tns --version");
-                _shell.Result(result.stdout, "Not Installed");
+                _shell.Result(result.stdout, "is not Installed");
             }
             catch (Exception Ex)
             {
